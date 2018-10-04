e785524bfb6c7526b3972e5d194079a93f95cec2
Kuznetsov Andrey <and.rey.q@yandex.ru>
2018-10-04 20:42:01 +0300

    Update rc_task_finally.js

--- tasks/task_1/rc_task_finally.js
+++ tasks/task_1/rc_task_finally.js
@@ -3,98 +3,33 @@ MESSAGE = "";
 RESULT = [];
 
 // org и division - cтроки с id
-org = org.split(',')	
-division = division.split(',')	
 
-temp = {
-	start: "for $elem in collaborators ",
-	fullname: "", 
-	position: "", 
-	org: "",
-	division: "",
-	where: "", 
-	and1: "", 
-	and2: "",
-	end: " return $elem"
+test = []
+if (fullname)
+{
+	test.push(" contains($elem/fullname, '"+ fullname +"') ")
+} 
+if (position)
+{
+	test.push(" contains($elem/position_name, '"+ position +"') ")
 }
 
-if (fullname) {
-	// ====
-	temp.where = " where "
-	// ====
-	temp.fullname = " contains($elem/fullname, '"+ fullname +"') " 
-} else {
-	temp.fullname = ""
+if (position)
+{
+	test.push(" MatchSome( $elem/position_parent_id, ( " + division + " ) ) " )	
+}
+ else if (org)
+{
+	test.push(" MatchSome( $elem/org_id, ( " + org + " ) ) " )
 }
 
-if (position) {
-	// ====
-	temp.where = " where "
-	if (temp.fullname != "") {
-		temp.and1 = " and "
-	}
-	// ====
-	temp.position = " contains($elem/position_name, '"+ position +"') " 
-} else {
-	temp.position = ""
-}
-
-if (org[0]) {
-	// ====
-	temp.where = " where " 	
-	if (temp.fullname != "" || temp.position != "") {
-		temp.and1 = " and "
-	}
-	if (temp.fullname != "" && temp.position != "") {
-		temp.and2 = " and "
-	}
-	// ====
-	if (org.length == 1) {
-		temp.org = " contains($elem/org_id, '"+ org[0] +"') "
-	} else {
-		// Если несколько организаций or contains
-		temp.org = " (contains($elem/org_id, '"+ org[0] +"') " 
-		for (i = 1, max = org.length; i < max; i++) {
-			temp.org += " or contains($elem/org_id, '"+ org[i] +"') "	
-		}
-		temp.org += ') '		
-	}
-} else {
-	temp.org = ""
-}
-
-if (division[0]) {
-	// ====
-	temp.where = " where " 	
-	temp.org = ""
-	if (temp.fullname != "" || temp.position != "") {
-		temp.and1 = " and "
-	}
-	if (temp.fullname != "" && temp.position != "") {
-		temp.and2 = " and "
-	}
-	// ====
-	if (division.length == 1) {
-		temp.division = " contains($elem/position_parent_id, '"+ division[0] +"') " 		
-	} else {
-		temp.division = " (contains($elem/position_parent_id, '"+ division[0] +"') " 
-		for (i = 1, max = division.length; i < max; i++) {
-			temp.division += " or contains($elem/position_parent_id, '"+ division[i] +"') "	
-		}
-		temp.division += ') '		
-	}
-} else {
-	temp.division = ""
-}
-
-str = temp.start + temp.where + temp.fullname + temp.and1 + temp.position + temp.and2 + temp.org + temp.division + temp.end 
-alert(str)
+str =  "for $elem in collaborators " + (test.lengh > 0 ? "where " + test.join(" and ") : "") + " return $elem"
 
 data = XQuery(str);
 
 for (elem in data)
 {	
-
+	// Не разбивать по пробелу
 	fullname = elem.fullname.Value.split(' ');
 	
 	RESULT.push({
